192fd3deae07a035d62d58e0e4aea0dc7e3fffd4
shoevarek <shoewarek@comcast.net>
2015-08-03 10:28:02 -0700

    Rejected commit: Default

--- app/src/main/java/hackaton/promotionsetup/activities/ListPromotions.java
+++ app/src/main/java/hackaton/promotionsetup/activities/ListPromotions.java
@@ -1,9 +1,11 @@
-package hackaton.promotionsetup;
+package hackaton.promotionsetup.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import hackaton.promotionsetup.R;
 
 public class ListPromotions extends Activity {
 
