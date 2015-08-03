5ea13347b41a73dd3bc982062f43ce85b2cdc8a2
shoevarek <shoewarek@comcast.net>
2015-08-03 15:07:39 -0700

    Initial commit

--- app/src/main/java/hackaton/promotionsetup/activities/ListPromotions.java
+++ app/src/main/java/hackaton/promotionsetup/activities/ListPromotions.java
@@ -1,23 +1,33 @@
 package hackaton.promotionsetup.activities;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Toast;
 
 import hackaton.promotionsetup.R;
 
-public class ListPromotions extends Activity {
+public class ListPromotions extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_promotions);
+
+        setListAdapter(mListAdapter);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+        super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.menu_list_promotions, menu);
         return true;
     }
@@ -34,6 +44,47 @@ public class ListPromotions extends Activity {
             return true;
         }
 
+        // TODO: create new intent and register it with view promotion summary action
+
         return super.onOptionsItemSelected(item);
     }
+
+    private BaseAdapter mListAdapter = new BaseAdapter() {
+        @Override
+        public int getCount() {
+            return 10;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position + 1;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup container) {
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(R.layout.list_item, container, false);
+            }
+
+            // Because the list item contains multiple touch targets, you should not override
+            // onListItemClick. Instead, set a click listener for each target individually.
+
+            convertView.findViewById(R.id.secondary_action).setOnClickListener(
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Toast.makeText(ListPromotions.this,
+                                    R.string.item_tooltip,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+            return convertView;
+        }
+    };
 }
